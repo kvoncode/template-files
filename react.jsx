e399e7cdcfa3cbf00e7a95e05a1f66e1131d08ee
winepkd <winepkd@gmail.com>
2019-04-07 12:00:32 +0900

    add: react template with bootstrap

--- react.jsx
+++ react.jsx
@@ -1,7 +1,7 @@
 //import React from 'react'
 //import ReactDOM from 'react-dom'
 
-const TITLE = "React";
+const TITLE = "Elon Musk Tribute Page";
 
 class App extends React.Component {
     componentDidMount() {
@@ -10,7 +10,33 @@ class App extends React.Component {
 
     render() {
         return (
-            <h1> Please change template</h1>
+
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">React with Bootstrap includes</h1>
+                    <h6>head</h6>
+                    <ul className="list-group">
+                        <li className="list-group-item">Charset and Viewport meta</li>
+                        <li className="list-group-item">Bootstrap CSS</li>
+                        <li className="list-group-item">Google Open Sans Font</li>
+                        <li className="list-group-item">Favicon meta and links</li>
+                        <li className="list-group-item">Font Awesome</li>
+                        <li className="list-group-item">Custom CSS</li>
+
+                    </ul>
+
+                    <h6>body</h6>
+
+                    <ul>
+
+                        <li className="list-group-item">jQuery first, then Popper.js, then Bootstrap JS</li>
+                        <li className="list-group-item">React and ReactDOM</li>
+                        <li className="list-group-item">Babel</li>
+                        <li className="list-group-item">Custom jsx</li>
+                    </ul>
+                </div>
+
+            </div>
         )
     }
 }
