837cfcbce51c02db9111471f3535a7c626c5b7e3
winepkd <winepkd@gmail.com>
2019-04-14 13:37:48 +0900

    update: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,42 @@
 const path = require('path');
 
 module.exports = {
-    entry: './html/tribute-page/src/index.jsx',
+    mode: "development",
+    entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'html/tribute-page', 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
