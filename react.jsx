e1b73d2840fcc0b7a1eab09b00138b4ce784d544
winepkd <winepkd@gmail.com>
2019-04-20 13:25:33 +0900

    update: react templates

--- react.jsx
+++ react.jsx
@@ -1,41 +1,14 @@
-//import React from 'react'
-//import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestMock from './components/test-mock/test-mock.jsx'
 
-const TITLE = "React with Bootstrap Template";
 
 class App extends React.Component {
-    componentDidMount() {
-        document.title = TITLE;
-    }
-
     render() {
         return (
-
-            <div className="jumbotron jumbotron-fluid">
-                <div className="container">
-                    <h1 className="display-4">React with Bootstrap includes</h1>
-                    <h6>head</h6>
-                    <ul className="list-group">
-                        <li className="list-group-item">Charset and Viewport meta</li>
-                        <li className="list-group-item">Bootstrap CSS</li>
-                        <li className="list-group-item">Google Open Sans Font</li>
-                        <li className="list-group-item">Favicon meta and links</li>
-                        <li className="list-group-item">Font Awesome</li>
-                        <li className="list-group-item">Custom CSS</li>
-
-                    </ul>
-
-                    <h6>body</h6>
-
-                    <ul>
-
-                        <li className="list-group-item">jQuery first, then Popper.js, then Bootstrap JS</li>
-                        <li className="list-group-item">React and ReactDOM</li>
-                        <li className="list-group-item">Babel</li>
-                        <li className="list-group-item">Custom jsx</li>
-                    </ul>
-                </div>
-
+            <div className="">
+                Hello fcc
+            <TestMock />
             </div>
         )
     }
@@ -45,3 +18,4 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
